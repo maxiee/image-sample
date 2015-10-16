d27637fad5b071a6cc92fba71ee5973d045d2a36
maxiee <maxieewong@gmail.com>
2015-10-16 17:30:36 +0800

    MainActivity.java: loading given images using Glide.

--- app/src/main/java/com/example/maxiee/image_sample/MainActivity.java
+++ app/src/main/java/com/example/maxiee/image_sample/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.maxiee.image_sample;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import com.bumptech.glide.Glide;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String[] IMAGE_URLS = new String[] {
+            "http://llss.qiniudn.com/forum/image/525d1960c008906923000001_1397820588.jpg",
+            "http://llss.qiniudn.com/forum/image/e8275adbeedc48fe9c13cd0efacbabdd_1397877461243.jpg",
+            "http://llss.qiniudn.com/uploads/forum/topic/attached_img/5350db2ffcfff258b500dcb2/_____2014-04-18___3.52.33.png"
+    };
+
+    private LinearLayout mMainLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,23 +28,35 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        mMainLayout = (LinearLayout) findViewById(R.id.main_layout);
+
+        showImages(IMAGE_URLS);
+    }
+
+    private void showImages(String[] urls) {
+        int margin = (int) getResources().getDimension(R.dimen.image_margin);
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        layoutParams.setMargins(margin, margin, margin, margin);
+        for (String url: urls) {
+            ImageView imageView = new ImageView(this);
+            mMainLayout.addView(imageView, layoutParams);
+            Glide.with(this).load(url).into(imageView);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
